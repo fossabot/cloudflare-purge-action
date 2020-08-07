1a769f96b740c5d9083bf8dca97c96f7891a5bf5
MC探索者i <mcseekeri@outlook.com>
2020-08-07 08:59:13 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,56 +2,7 @@
 
 set -e
 
-######## Check for required/optional inputs. ########
 
-# Determine whether using a Global API Key or a restricted API Token.
-if [ -n "$CLOUDFLARE_KEY" ]; then
-  # If they've passed a key, the account email address is also required.
-  if [ -n "$CLOUDFLARE_EMAIL" ]; then
-    API_METHOD=1
-  else
-    echo "CLOUDFLARE_EMAIL is required when using a Global API Key. Quitting."
-    exit 1
-  fi
-
-# No key was entered, check if they're using a token.
-elif [ -n "$CLOUDFLARE_TOKEN" ]; then
-  API_METHOD=2
-
-# The user hasn't entered either a key or a token, can't do anything else.
-else
-  echo "Looks like you haven't set the required authentication variables."
-  echo "Check out the README for options: https://git.io/JeBbD"
-  exit 1
-fi
-
-
-# Check if Zone ID is set.
-if [ -z "$CLOUDFLARE_ZONE" ]; then
-  echo "CLOUDFLARE_ZONE is not set. Quitting."
-  exit 1
-fi
-
-# If URL array is passed, only purge those. Otherwise, purge everything.
-if [ -n "$PURGE_URLS" ]; then
-  set -- --data '{"files":'"${PURGE_URLS}"'}'
-else
-  set -- --data '{"purge_everything":true}'
-fi
-
-
-######## Call the API and store the response for later. ########
-
-# Using a global API key:
-if [ "$API_METHOD" -eq 1 ]; then
-  HTTP_RESPONSE=$(curl -sS "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE}/purge_cache" \
-                      -H "Content-Type: application/json" \
-                      -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" \
-                      -H "X-Auth-Key: ${CLOUDFLARE_KEY}" \
-                      -w "HTTP_STATUS:%{http_code}" \
-                      "$@")
-
-# Using an API token:
 elif [ "$API_METHOD" -eq 2 ]; then
   HTTP_RESPONSE=$(curl -sS "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE}/purge_cache" \
                       -H "Content-Type: application/json" \
